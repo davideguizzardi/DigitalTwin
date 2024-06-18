f9cbff539975f2f6e9dfee937e886e1ee77d465a
Murotek <m.muratore001@studenti.unibs.it>
2024-06-18 15:55:18 +0200

    feat(upload map client)
    button to remove a map from the client

--- resources/js/Components/ConfigurationMap.jsx
+++ resources/js/Components/ConfigurationMap.jsx
@@ -1,8 +1,8 @@
-import { Button } from 'flowbite-react';
 import React, { useCallback, useMemo, useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { ThemeButton } from './ThemeButton';
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus, FaTrashCan} from "react-icons/fa6";
+import { Button } from 'flowbite-react';
 
 const baseStyle = {
     display: 'flex',
@@ -41,7 +41,7 @@ export default function ConfigurationMap({ props }) {
         setFiles([...files, Object.assign(firstFile, {
             preview: URL.createObjectURL(firstFile)
         })]);
-        setListMap(listMap => [...listMap, acceptedFiles.at(0)]);
+        setListMap(listMap => [...listMap, firstFile]);
         counter++;
         setIndexThumbs(counter - 1);
     }, []);
@@ -69,7 +69,7 @@ export default function ConfigurationMap({ props }) {
 
 
     const buttons = listMap.map((map, index) => (
-        <ThemeButton className='size-min m-2' onClick={() => { setIndexThumbs(index);console.log(listMap);}}
+        <ThemeButton className='size-min mx-px my-2' onClick={() => { setIndexThumbs(index);console.log(listMap);}}
             pill key={map.name}>{index}</ThemeButton>
     ));
     const dragAndDrop = (
@@ -79,8 +79,15 @@ export default function ConfigurationMap({ props }) {
         </div>
     );
     const thumbs = listMap.map(file => (
-        <div className='w-full justify-center' key={file.name}>
-            <img className='max-w-full max-h-96 aspect-square'
+        <div className='relative size-full flex items-center justify-center' key={file.name}>
+            <Button className='absolute right-0 top-0 bg-red-500 enabled:hover:bg-red-700' pill
+                onClick={() => {
+                    setListMap(listMap.filter(f => f.name !== file.name));
+                    setIndexThumbs(counter--);
+                }}>
+                <FaTrashCan className='size-4' color='white'/>
+            </Button>
+            <img className='max-w-full max-h-96'
                 src={file.preview}
                 alt={file.name}
             />
@@ -94,19 +101,22 @@ export default function ConfigurationMap({ props }) {
 
 
     return (
-        <div className="w-3/6 bg-white shadow items-center flex flex-col">
-            <h1>Configure map house</h1>
-            <div className="flex items-center w-full h-full p-3" >
-                <div className="w-full h-full">
+        <div className="w-3/6 bg-white shadow items-center flex flex-col justify-around">
+            <p className='text-2xl'>Configure map house</p>
+            <div className="flex items-center w-full p-3" >
+                <div className="w-full h-96">
                     {indexThumbs < 0 ? dragAndDrop : thumbs.at(indexThumbs)}
                 </div>
-                <div className="flex flex-col justify-center w-min m-2 p-2 rounded-full bg-gray-100">
+                <div className="flex flex-col justify-center w-min m-2 p-1 rounded-full bg-gray-100 shadow">
                     {buttons}
-                    <ThemeButton className='size-min m-2' pill onClick={() => { setIndexThumbs(-1), console.log(indexThumbs) }} >
+                    <ThemeButton className='size-min mx-px my-2' pill onClick={() => { setIndexThumbs(-1), console.log(indexThumbs) }} >
                         <FaPlus />
                     </ThemeButton>
                 </div>
             </div>
+            <ThemeButton>
+                Upload
+            </ThemeButton>
         </div>
     );
 }
