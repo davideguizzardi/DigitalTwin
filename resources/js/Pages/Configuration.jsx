0b942d1d56d15076c6fdfd8e22bcf4bbbe43fd72
Murotek <m.muratore001@studenti.unibs.it>
2024-06-19 17:19:57 +0200

    fix(Configuration)
    correct old tag

--- resources/js/Pages/Configuration.jsx
+++ resources/js/Pages/Configuration.jsx
@@ -1,10 +1,9 @@
 import UploadMap from '@/Components/UploadMap';
-import UserLayout from '@/Layouts/UserLayout';
 
 const Configuration = ({maps}) => {
     console.log(maps)
     return (
-        <ConfigurationMap/>
+        <UploadMap/>
     )
 }
 
