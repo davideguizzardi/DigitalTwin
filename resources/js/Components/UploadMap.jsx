b8fc9a24e0ec165d5f844ac06a97597dac283dc0
Murotek <m.muratore001@studenti.unibs.it>
2024-06-20 13:08:49 +0200

    ref(UploadMap)
    aggiunto il component dragDropFile

--- resources/js/Components/UploadMap.jsx
+++ resources/js/Components/UploadMap.jsx
@@ -4,47 +4,21 @@ import { ThemeButton } from './ThemeButton';
 import { FaPlus, FaTrashCan } from "react-icons/fa6";
 import { Button } from 'flowbite-react';
 import { router } from '@inertiajs/react';
+import DragDropFile from './DragDropFile';
 
-const baseStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '20px',
-    borderWidth: 2,
-    borderRadius: 2,
-    borderColor: '#eeeeee',
-    borderStyle: 'dashed',
-    backgroundColor: '#fafafa',
-    color: '#bdbdbd',
-    transition: 'border .3s ease-in-out'
-};
-
-const activeStyle = {
-    borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-    borderColor: '#00e676'
-};
-
-const rejectStyle = {
-    borderColor: '#ff1744'
-};
-
-let counter = 0;
+let counter = -1;
 
 export default function UploadMap({ props }) {
-    let [files, setFiles] = useState([]);
     let [listMap, setListMap] = useState([]);
     let [indexThumbs, setIndexThumbs] = useState(-1);
+
     const onDrop = useCallback(acceptedFiles => {
         const firstFile = acceptedFiles.at(0);
-        setFiles([...files, Object.assign(firstFile, {
+        setListMap(listMap => [...listMap, Object.assign(firstFile,{
             preview: URL.createObjectURL(firstFile)
         })]);
-        setListMap(listMap => [...listMap, firstFile]);
         counter++;
-        setIndexThumbs(counter - 1);
+        setIndexThumbs(counter);
     }, []);
 
     const { getRootProps,
@@ -54,31 +28,14 @@ export default function UploadMap({ props }) {
         isDragReject
     } = useDropzone({
         onDrop,
-        accept: 'image/png, image/jpeg'
+        accept: 'image/*'
     })
-
-    const style = useMemo(() => ({
-        ...baseStyle,
-        ...(isDragActive ? activeStyle : {}),
-        ...(isDragAccept ? acceptStyle : {}),
-        ...(isDragReject ? rejectStyle : {})
-    }), [
-        isDragActive,
-        isDragReject,
-        isDragAccept
-    ]);
-
 
     const buttons = listMap.map((map, index) => (
         <ThemeButton className='size-min mx-px my-2' onClick={() => { setIndexThumbs(index); console.log(listMap); }}
             pill key={map.name}>{index}</ThemeButton>
     ));
-    const dragAndDrop = (
-        <div className='h-full' {...getRootProps({ style })}>
-            <input {...getInputProps()} />
-            <div>Drag and drop your images here.</div>
-        </div>
-    );
+
     const thumbs = listMap.map(file => (
         <div className='relative size-full flex items-center justify-center' key={file.name}>
             <Button className='absolute right-0 top-0 bg-red-500 enabled:hover:bg-red-700' pill
@@ -96,6 +53,7 @@ export default function UploadMap({ props }) {
             />
         </div>
     ));
+
     const submit = () => {
         const formData = new FormData();
         formData.append('maps', listMap);
@@ -109,8 +67,8 @@ export default function UploadMap({ props }) {
 
     // clean up
     useEffect(() => {
-        files.forEach(file => URL.revokeObjectURL(file.preview));
-    }, [files, listMap, indexThumbs]);
+        listMap.forEach(file => URL.revokeObjectURL(file.preview));
+    }, [listMap]);
 
 
     return (
@@ -119,7 +77,10 @@ export default function UploadMap({ props }) {
             <form name="maps" className='h-full w-full flex flex-col justify-center py-2' onSubmit={submit}>
                 <div className="flex items-center w-full px-3 py-5" >
                     <div className="w-full h-96">
-                        {indexThumbs >= 0 ? thumbs.at(indexThumbs) : dragAndDrop}
+                        {indexThumbs >= 0 ? thumbs.at(indexThumbs) : 
+                        <DragDropFile getRootProps={getRootProps} getInputProps={getInputProps}
+                        isDragAccept={isDragAccept} isDragActive={isDragActive} isDragReject={isDragReject}/> }
+
                     </div>
                     <div className="flex flex-col justify-center w-min m-2 p-1 rounded-full bg-gray-100 shadow">
                         {buttons}
