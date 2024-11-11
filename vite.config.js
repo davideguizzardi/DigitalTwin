fe97b5adb154c94398b7a79f8dde855e37ea6098
Murotek <m.muratore001@studenti.unibs.it>
2024-11-11 19:52:58 +0100

    ops(vite)
    configure server for CORS erros

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        origin: 'http://localhost:5173',
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
